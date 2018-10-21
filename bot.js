c7cb68143ee6e25c7b41789efc7c857f5a675eb5
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-10-22 01:08:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,15 +14,4 @@ var channel = "503686842206650383";
     },305);
 })
 
-client.on('message',function(message) {
-    let prefix = ""; //بريفيكسك
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-  if (message.author.id !== '321463690815602689') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
-  if(!message.author.id === '321463690815602689') return;
-if(!args) return;
-message.channel.send(`${args}`);
-}
-});
-
 client.login(process.env.BOT_TOKEN); 
